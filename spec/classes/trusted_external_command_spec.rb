7680bcc0b010620d026dbbfc9a1dd3708c29e77d
Enis Inan <enis.inan@puppet.com>
2020-05-27 13:05:05 -0700

    (maint) Define Service['pe-puppetserver'] if it is undefined
    This improves the 'puppet apply' UX.
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- spec/classes/trusted_external_command_spec.rb
+++ spec/classes/trusted_external_command_spec.rb
@@ -3,12 +3,6 @@
 require 'spec_helper'
 
 describe 'servicenow_integration::trusted_external_command' do
-  let(:pre_condition) do
-    <<-MANIFEST
-service { 'pe-puppetserver':
-}
-    MANIFEST
-  end
   let(:params) do
     {
       'instance' => 'foo_instance',
@@ -18,5 +12,18 @@ service { 'pe-puppetserver':
     }
   end
 
-  it { is_expected.to compile }
+  context "when Service['pe-puppetserver'] is defined" do
+    let(:pre_condition) do
+      <<-MANIFEST
+service { 'pe-puppetserver':
+}
+      MANIFEST
+    end
+
+    it { is_expected.to compile }
+  end
+
+  context "when Service['pe-puppetserver'] is undefined" do
+    it { is_expected.to compile }
+  end
 end
