cf4443e0e23f9c38004ba2172e65cd376ef2d2f0
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-05-11 12:21:26 +0800

    Nicer way to to code Signed-off-by: Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>

--- files/servicenow.rb
+++ files/servicenow.rb
@@ -173,9 +173,12 @@ def servicenow(certname, config_file = nil)
   if factnameinplaceofcertname  
     Puppet.initialize_settings if Puppet.settings[:vardir].nil? || Puppet.settings[:vardir].to_s.empty?
     valuetolinkCMBD=Facter.value(factnameinplaceofcertname)
-    data=%x(
+    
+    cmdata= <<-CMDATA
       certname=#{certname}; q=\"inventory[facts.#{factnameinplaceofcertname}]{ certname = \\\"$certname\\\" }\" ; sn=`/opt/puppetlabs/bin/puppet config print server` ; puppet query "$q"  --urls https://${sn}:8081  --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem  --cert /etc/puppetlabs/puppet/ssl/certs/${sn}.pem  --key /etc/puppetlabs/puppet/ssl/private_keys/${sn}.pem
-    )
+CMDATA
+    data=%x(#{cmdata})
+    
     valuetolinkCMBD=JSON.parse(data)[0].values[0]
   else
     valuetolinkCMBD=certname
